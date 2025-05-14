eb4ba3d26de0b4515867eadc3881631bd7bd1c60
itsmk42 <ballery619@gmail.com>
2025-05-14 11:10:34 +0530

    Initial commit of promptKat application

--- push-to-github.sh
+++ push-to-github.sh
@@ -12,6 +12,9 @@ git commit -m "Initial commit of promptKat application"
 # Add remote repository
 git remote add origin https://github.com/itsmk42/promptcat.git
 
+# Create main branch if it doesn't exist
+git branch -M main
+
 # Push to GitHub
 git push -u origin main
 
